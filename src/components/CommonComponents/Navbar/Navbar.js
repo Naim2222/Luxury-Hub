4c866889f1179fa097f49204372b49d24b4a471c
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-12-06 18:51:42 +0600

    final commit

--- src/components/CommonComponents/Navbar/Navbar.js
+++ src/components/CommonComponents/Navbar/Navbar.js
@@ -12,7 +12,6 @@ function Navbar() {
                         <img src={Logo} alt="" />
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        {/* <span className="navbar-toggler-icon"></span> */}
                         <MenuIcon className="menu_icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
